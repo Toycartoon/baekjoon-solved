aee2eb9ad28d5c98e08930fbb3cf4486fa5fa07b
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-03-30 01:01:29 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
+++ Python/백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -1,12 +1,29 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-dp = [0 for _ in range(n)]
-for i in range(n-1, -1, -1):
-    dp[i] = 1
-    for j in range(n-1, i-1, -1):
-        if a[i] > a[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+def binary_search(l, r, i):
+    while l < r:
+        mid = (l + r) // 2
+
+        if lis[mid] < i:
+            l = mid + 1
+        elif lis[mid-1] < i <= lis[mid]:
+            return mid
+        else:
+            r = mid - 1
+
+    return l
 
 
-print(max(dp))
+a.reverse()
+
+lis = [a[0]]
+for i in range(1, n):
+    if lis[-1] < a[i]:
+        lis.append(a[i])
+    else:
+        idx = binary_search(0, len(lis)-1, a[i])
+        lis[idx] = a[i]
+
+
+print(len(lis))
