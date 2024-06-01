f90b0f92359b468ca988b8fc4415d87f76b24f0c
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-06-02 04:12:52 +0900

    [Bronze III] Title: Character Frequencies, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/29847. Character Frequencies/Character Frequencies.py
@@ -0,0 +1,14 @@
+n = int(input())
+s = {}
+for _ in range(n):
+    f = input()
+    for i in f:
+        if i == " ":
+            continue
+        try:
+            s[i] += 1
+        except KeyError:
+            s[i] = 1
+
+for i in sorted(s.keys()):
+    print(i, s[i])
