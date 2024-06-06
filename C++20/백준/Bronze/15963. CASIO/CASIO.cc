b9a3d41f727b22ad749877f9c4428a13cf08f2c4
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-06-07 04:25:59 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long a, b;
+
+    cin >> a >> b;
+    if(a == b)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+
+    return 0;
+}
