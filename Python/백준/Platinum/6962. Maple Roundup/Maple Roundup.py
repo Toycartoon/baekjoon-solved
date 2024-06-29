6f31412390e5a86af1a0e442fd7efe01b2d66241
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-06-30 02:51:44 +0900

    [Platinum V] Title: Maple Roundup, Time: 68 ms, Memory: 33240 KB -BaekjoonHub

---
+++ Python/백준/Platinum/6962. Maple Roundup/Maple Roundup.py
@@ -0,0 +1,39 @@
+import sys
+from math import sqrt
+
+input = sys.stdin.readline
+
+def ccw(a, b, c):
+    u = (b[0] - a[0], b[1] - a[1])
+    v = (c[0] - b[0], c[1] - b[1])
+
+    return u[0] * v[1] - u[1] * v[0]
+
+
+def convex_hull(arr):
+    arr.sort()
+
+    upper = []
+    for p in arr:
+        while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) >= 0:
+            upper.pop()
+        upper.append(p)
+
+    lower = []
+    for p in reversed(arr):
+        while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) >= 0:
+            lower.pop()
+        lower.append(p)
+
+    return upper[:-1] + lower[:-1]
+
+
+for t in range(int(input())):
+    n = int(input())
+    arr = [tuple(map(int, input().split())) for _ in range(n)]
+    ans = convex_hull(arr)
+    a = 0
+    for i in range(len(ans)):
+        a += sqrt((ans[i-1][0] - ans[i][0]) ** 2 + (ans[i-1][1] - ans[i][1]) ** 2)
+
+    print("{:.2f}".format(a))
