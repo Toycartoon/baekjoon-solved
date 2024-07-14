102a3198c06e0fba3c4a9c44b2e709cffb8a920d
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-07-15 02:14:49 +0900

    [Gold I] Title: 구간 합 구하기, Time: 872 ms, Memory: 118500 KB -BaekjoonHub

---
+++ Python/백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -0,0 +1,55 @@
+# Special Thanks : jshyun912, jms020820
+import sys
+
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+tree = [0 for _ in range(n * 2)]
+
+
+def update(tree, i, val):
+    i += len(tree) // 2 - 1
+    tree[i] = val
+
+    while i > 1:
+        i //= 2
+        tree[i] = tree[i * 2] + tree[i * 2 + 1]
+
+
+def query(tree, i, j):
+    ans = 0
+
+    i += len(tree) // 2 - 1
+    j += len(tree) // 2 - 1
+
+    while i <= j:
+        if i % 2 == 1:
+            ans += tree[i]
+            i += 1
+        if j % 2 == 0:
+            ans += tree[j]
+            j -= 1
+
+        i //= 2
+        j //= 2
+
+    return ans
+
+
+for i in range(n):
+    tree[n + i] = int(input())
+
+
+n = len(tree) // 2
+
+for i in range(n-1, 0, -1):
+    tree[i] = tree[i * 2] + tree[i * 2 + 1]
+
+
+for i in range(m + k):
+    a, b, c = map(int, input().split())
+
+    if a == 1:
+        update(tree, b, c)
+    elif a == 2:
+        print(query(tree, b, c))
