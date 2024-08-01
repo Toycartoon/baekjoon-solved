925a6e3a24299c7e3f16d28c08e9ed6fec0af612
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-08-01 20:56:05 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,11 @@
+n = int(input())
+dp = [[0] * 10 for _ in range(n)]
+
+for i in range(10):
+    dp[0][i] = 1
+
+for i in range(1, n):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][:j+1]) % 10007
+
+print(sum(dp[n-1]) % 10007)
