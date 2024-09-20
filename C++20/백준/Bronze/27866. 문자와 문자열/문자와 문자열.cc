5d1484405be87a9eaa4876d0b8c5c7b0f67006ce
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-09-21 03:05:12 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    int i;
+
+    cin >> s;
+    cin >> i;
+
+    cout << s[i-1] << endl;
+
+    return 0;
+}
