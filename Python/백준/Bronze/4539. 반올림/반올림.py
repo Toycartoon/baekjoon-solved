0970fdc13eaf7a90637b8602f8f4d50b5bbaf93d
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2024-11-29 23:35:15 +0900

    [Bronze I] Title: 반올림, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/4539. 반올림/반올림.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+for t in range(int(input())):
+    x = list(map(int, [*input().strip()]))
+
+    if len(x) == 1:
+        print(x[0])
+        continue
+
+    for i in range(len(x)-2, -1, -1):
+        if x[i+1] >= 5:
+            x[i] += 1
+        x[i+1] = 0
+
+    for i in x:
+        print(i, end="")
+    print()
