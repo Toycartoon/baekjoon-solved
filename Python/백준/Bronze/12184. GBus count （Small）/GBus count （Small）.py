cb424adc7259ae43e689a526953d0559b1203eda
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-02-04 18:28:37 +0900

    [Bronze II] Title: GBus count (Small), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/12184. GBus count （Small）/GBus count （Small）.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+mt = int(input())
+for t in range(mt):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    c = []
+    for i in range(int(input())):
+        c.append(int(input()))
+
+
+    print(f"Case #{t+1}: ", end='')
+
+    for i in c:
+        ans = 0
+        for x in range(0, n*2, 2):
+            if a[x] <= i <= a[x+1]:
+                ans += 1
+
+        print(ans, end=" ")
+    print()
+    input()
