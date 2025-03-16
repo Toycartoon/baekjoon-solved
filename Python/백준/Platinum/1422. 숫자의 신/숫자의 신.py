ef6ed91ba57d140afef8893fb17a68eb2c86f2f5
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-03-16 10:43:43 +0900

    [Platinum IV] Title: 숫자의 신, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ Python/백준/Platinum/1422. 숫자의 신/숫자의 신.py
@@ -0,0 +1,25 @@
+import sys
+from math import lcm
+
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+l = []
+a = set()
+
+for i in range(k):
+    x = input().strip()
+    l.append(x)
+    a.add(len(x))
+
+l.sort(key=lambda x: int(x))
+u = l
+for i in range(n-k):
+    u.append(l[-1])
+
+c = lcm(*a)
+for i in range(10):
+    u = sorted(u, key=lambda x: (c * x), reverse=True)
+
+for i in u:
+    print(i, end="")
