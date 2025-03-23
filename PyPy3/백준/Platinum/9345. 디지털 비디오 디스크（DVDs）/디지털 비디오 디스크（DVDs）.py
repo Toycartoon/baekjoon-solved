26ad833b21aa8fe62a005c5f6f314eb83431c1a0
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-03-24 04:02:20 +0900

    [Platinum III] Title: 디지털 비디오 디스크(DVDs), Time: 2028 ms, Memory: 233900 KB -BaekjoonHub

---
+++ PyPy3/백준/Platinum/9345. 디지털 비디오 디스크（DVDs）/디지털 비디오 디스크（DVDs）.py
@@ -0,0 +1,65 @@
+import sys
+from math import inf
+
+input = sys.stdin.readline
+
+
+def update(tree, i, j):
+    i += len(tree) // 2 - 1
+    j += len(tree) // 2 - 1
+    tree[i], tree[j] = tree[j], tree[i]
+
+    while i > 1:
+        i //= 2
+        tree[i] = (min(tree[i * 2][0], tree[i * 2 + 1][0]), max(tree[i * 2][1], tree[i * 2 + 1][1]),
+                   tree[i * 2][2] + tree[i * 2 + 1][2])
+
+    while j > 1:
+        j //= 2
+        tree[j] = (min(tree[j * 2][0], tree[j * 2 + 1][0]), max(tree[j * 2][1], tree[j * 2 + 1][1]),
+                   tree[j * 2][2] + tree[j * 2 + 1][2])
+
+def query(tree, i, j):
+    ans = (inf, 0, 0)
+
+    i += len(tree) // 2 - 1
+    j += len(tree) // 2 - 1
+
+    while i <= j:
+        if i % 2 == 1:
+            ans = (min(tree[i][0], ans[0]), max(tree[i][1], ans[1]), ans[2] + tree[i][2])
+            i += 1
+        if j % 2 == 0:
+            ans = (min(tree[j][0], ans[0]), max(tree[j][1], ans[1]), ans[2] + tree[j][2])
+            j -= 1
+
+        i //= 2
+        j //= 2
+
+    return ans
+
+
+for t in range(int(input())):
+    n, k = map(int, input().split())
+    tree = [(inf, 0, 0) for x in range(n)]
+    for x in range(n):
+        tree.append((x, x, x))
+
+    n = len(tree) // 2
+    for i in range(n-1, 0, -1):
+        tree[i] = (min(tree[i * 2][0], tree[i * 2 + 1][0]), max(tree[i * 2][1], tree[i * 2 + 1][1]),
+                   tree[i * 2][2] + tree[i * 2 + 1][2])
+
+
+    for x in range(k):
+        q, a, b = map(int, input().split())
+
+        if q == 0:
+            update(tree, a+1, b+1)
+        elif q == 1:
+            v = query(tree, a+1, b+1)
+
+            if v[0] == a and v[1] == b and v[2] == (b - a + 1) * (a + b) // 2:
+                print("YES")
+            else:
+                print("NO")
