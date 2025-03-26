692f2f5f6a9b055262817e90d451927a5a7ae319
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-03-26 23:38:39 +0900

    [Silver I] Title: 소수를 분수로, Time: 100 ms, Memory: 38936 KB -BaekjoonHub

---
+++ Python/백준/Silver/5376. 소수를 분수로/소수를 분수로.py
@@ -0,0 +1,27 @@
+import re
+import sys
+from math import gcd, lcm
+
+input = sys.stdin.readline
+
+def repeating_decimal(x):
+    deno = int("9" * len(x.split("(")[1][:-1]) + "0" * len(x.split(".")[1].split("(")[0]))
+    mole = int(re.sub(r"\D+", "", x)) - int(re.sub(r"\D+", "", x.split("(")[0]))
+
+    return mole, deno
+
+
+def decimal(x):
+    return int(re.sub(r"\D+", "", x)), 10 ** len(x.split(".")[1])
+
+
+for t in range(int(input())):
+    s = input().strip()
+
+    if "(" in s:
+        frac_s = repeating_decimal(s)
+    else:
+        frac_s = decimal(s)
+
+    g = gcd(frac_s[0], frac_s[1])
+    print(f"{frac_s[0] // g}/{frac_s[1] // g}")
