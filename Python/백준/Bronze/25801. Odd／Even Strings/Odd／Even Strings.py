0358c28931d1db3f928383d23c95d08a8e1a4fe5
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-04-01 15:10:54 +0900

    [Bronze III] Title: Odd/Even Strings, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/25801. Odd／Even Strings/Odd／Even Strings.py
@@ -0,0 +1,16 @@
+w = [0 for _ in range(26)]
+s = input()
+
+for i in s:
+    w[ord(i)-97] += 1
+
+f = w[ord(s[0])-97] % 2
+for i in w:
+    if i == 0:
+        continue
+
+    if i % 2 != f:
+        f = "0/1"
+        break
+
+print(f)
