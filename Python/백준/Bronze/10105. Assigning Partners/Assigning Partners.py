511ffdad703919db4f0fdc06207ee503f8eee6e4
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-04-04 02:03:03 +0900

    [Bronze I] Title: Assigning Partners, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10105. Assigning Partners/Assigning Partners.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = input().split()
+b = input().split()
+
+w = {}
+for i in range(len(a)):
+    w[a[i]] = b[i]
+
+f = True
+for i in w:
+    if i != w[w[i]] or i == w[i]:
+        f = False
+
+if f:
+    print("good")
+else:
+    print("bad")
