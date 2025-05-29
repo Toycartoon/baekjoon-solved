439b8b42f7925de9ddba60a2ac21d08bbec88ca4
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-05-30 02:53:09 +0900

    [Silver II] Title: 병사 배치하기, Time: 604 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [0 for _ in range(n)]
+for i in range(n-1, -1, -1):
+    dp[i] = 1
+    for j in range(n-1, i-1, -1):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n-max(dp))
