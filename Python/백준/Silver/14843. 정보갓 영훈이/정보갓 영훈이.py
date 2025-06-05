b65b5cc2769354eb670efbe4066988838789a1b4
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-06-05 13:29:07 +0900

    [Silver V] Title: 정보갓 영훈이, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/14843. 정보갓 영훈이/정보갓 영훈이.py
+++ Python/백준/Silver/14843. 정보갓 영훈이/정보갓 영훈이.py
@@ -5,15 +5,15 @@ for i in range(n):
     s, a, t, m = map(float, input().split())
     ans += (s * (1 + (1 / a)) * (1 + (m / t))) / 4
 
-l = [(ans, "y")]
+l = [ans]
 p = int(input())
 for i in range(p):
-    l.append((float(input()), "x"))
+    l.append(float(input()))
 
 s = "Younghoon"
 
 l.sort(reverse=True)
-if (l.index((ans, "y")) + 1) / (p + 1) * 100 <= 15:
+if (l.index(ans) + 1) / (p + 1) * 100 <= 15:
     s += " \"The God\""
 
 print(f"The total score of {s} is {ans:.02f}.")
