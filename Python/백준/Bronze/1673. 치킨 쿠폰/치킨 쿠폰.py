dc8837eb6c49d7576bd594cd5eb2486a2f33a116
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-06-09 23:16:38 +0900

    [Bronze II] Title: 치킨 쿠폰, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1673. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        n, k = map(int, input().split())
+        ans = n
+
+        while n // k:
+            ans += n // k
+            n = n % k + n // k
+
+        print(ans)
+    except EOFError:
+        break
