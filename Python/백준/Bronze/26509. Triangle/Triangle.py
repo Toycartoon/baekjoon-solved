0c7072896370671e933d316c7fdc558c5e3f84d4
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-06-20 15:59:02 +0900

    [Bronze III] Title: Triangle, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/26509. Triangle/Triangle.py
@@ -0,0 +1,9 @@
+for n in range(int(input())):
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a.sort()
+    if a == sorted(b) and a[0] ** 2 + a[1] ** 2 == a[2] ** 2:
+        print("YES")
+    else:
+        print("NO")
