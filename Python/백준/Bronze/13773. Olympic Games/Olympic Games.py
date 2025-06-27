cd017e360a58edcd4645a80c284cad5aeaee7350
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-06-27 15:58:57 +0900

    [Bronze III] Title: Olympic Games, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/13773. Olympic Games/Olympic Games.py
@@ -0,0 +1,15 @@
+while True:
+    n = int(input())
+
+    if n == 0:
+        break
+
+    if n % 4 == 0 and n >= 1896:
+        if 1914 <= n <= 1918 or 1939 <= n <= 1945:
+            print(n, "Games cancelled")
+        elif n > 2020:
+            print(n, "No city yet chosen")
+        else:
+            print(n, "Summer Olympics")
+    else:
+        print(n, "No summer games")
